983605226d4493dcc49346e4a38f1d6c636238b9
Eva Simon <eva.simon@gmail.com>
2017-10-29 11:40:29 -0500

    model set up

---
+++ models/ice_cream.js
@@ -0,0 +1,9 @@
+// imports orm form config folder
+var orm = require('../config/orm');
+
+var ice_cream = {
+
+}
+// create the code that will call the ORM functions using burger specific input for the ORM.
+// * Export at the end of the `burger.js` file.
+module.exports = ice_cream;
