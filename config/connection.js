95b95b0b0c74f5741e1b96c20ec6ff56efd081e2
Eva Simon <eva.simon@gmail.com>
2017-10-29 21:48:39 -0500

    fixes connection

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ var connection;
 
 // sets connection param for database connection
 // and hooking the project with JawsDB
-if (process.env.JAWDB_URL) {
-    connection = mysql.createConnection(process.envJAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
